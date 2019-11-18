51c61fb137f9f083e8424597e06a2028f1c64711
Adrian Lorenzo <lorenzyadrian@gmail.com>
2019-11-18 14:47:47 +0800

    Master Commit

---
+++ index.js
@@ -0,0 +1,73 @@
+
+const numOfDrum = document.querySelectorAll(".drum").length;
+
+//Detect button clicks
+for(let i = 0; i<numOfDrum; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        let buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+        buttonAnimate(buttonInnerHTML);
+    }
+    )}
+
+    //Detect keyboard press. event = pressed key attributes (Check console log for attributes)
+    document.addEventListener("keydown", function(event) {
+        makeSound(event.key);
+        buttonAnimate(event.key);
+    })
+
+
+        function makeSound(key){
+        switch (key) {
+            case "w":
+                var crash = new Audio("/sounds/crash.mp3");
+                crash.play();
+                break;
+
+            case "a":
+                var kick = new Audio("/sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            
+            case "s":
+                var snare = new Audio("/sounds/snare.mp3");
+                snare.play();
+                break;
+            
+            case "d":
+                var tom1 = new Audio("/sounds/tom-1.mp3");
+                tom1.play();
+                break;
+
+            case "j":
+                var tom2 = new Audio("/sounds/tom-2.mp3");
+                tom2.play();
+                break;
+
+            case "k":
+                var tom3 = new Audio("/sounds/tom-3.mp3");
+                tom3.play();
+                break;
+
+            case "l":
+                var tom4 = new Audio("/sounds/tom-4.mp3");
+                tom4.play();
+                break;
+        
+            default:
+                break;
+        }
+    }
+    
+    function buttonAnimate(currentKey){
+        let activeButton = document.querySelector("." + currentKey);
+
+        activeButton.classList.add("pressed");
+
+        setTimeout(function() {
+            activeButton.classList.remove("pressed");
+        }, 100);
+
+    }
